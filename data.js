855d5dfe8a42ae43066fda4fbfa6ac40563eb308
Shreshth Mohan <sremog@gmail.com>
2020-05-16 21:15:45 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    name: "Indore",
+    notes: "Too big?",
+    population: "",
+    AQI: "",
+    temperature: "",
+    medicalFacilities: "",
+    costOfLiving: "",
+    localGotchas: "",
+    peacefulNeighborhoods: "",
+    internet: "",
+    nearestBigCity: "",
+    outsiderFriendly: ""
+    // child care
+  },
+  { name: "bhopal" },
+  { name: "Shillong" },
+  { name: "Bir" },
+  { name: "Ranikhet" }, // Aditya Babbar  - @xAditya
+  { name: "Jaipur" },
+  { name: "Dharamshala/McLeodganj" }, // dharamkot
+  { name: "Manali" }, // long term settlement for old people
+  { name: "Nainital" },
+  { name: "Kodi Bengre" },
+  { name: "Udupi" },
+  { name: "Manipal" },
+  { name: "Coimbatore" },
+  { name: "Auroville" },
+  { name: "Rishikesh" }, // jhoola area
+  { name: "Uttarkashi" },
+  { name: "Mysore" },
+  { name: "chandigarh" },
+  { name: "Salem" },
+  { name: "Nagpur" },
+  { name: "Goa" },
+  { name: "Jamshedpur" },
+  { name: "" }
+];
+
+// https://economictimes.indiatimes.com/small-biz/startups/features/giving-up-starting-up-whats-motivating-boomerangs-to-take-chance/articleshow/71457298.cms?from=mdr
+// https://twitter.com/sundermanbegins/status/1225294873055485954
+
+// Notes from nomadlist:
+// 1. Temperature in F!
+// 2. text on top of images doesn't always work
